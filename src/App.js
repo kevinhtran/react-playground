62356e816b47f4099c94770899157fcb59e2a7a1
kevinhtran <kevdeveloper7@gmail.com>
2021-09-17 17:42:29 -0700

    building an image slider. used hooks, back function to return ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
+// const cars = ['./images/interior-back.jpeg',
+// './images/interior-front.jpeg',
+// './images/exterior-back.jpeg',
+// './images/exterior-front.jpeg']
+
+const cars = [
+  {
+    id: 1,
+    path: "./images/interior-back.jpeg",
+  },
+  {
+    id: 2,
+    path: "./images/interior-front.jpeg",
+  },
+  {
+    id: 3,
+    path: "./images/exterior-back.jpeg",
+  },
+  {
+    id: 4,
+    path: "./images/exterior-front.jpeg",
+  },
+];
 function App() {
+
+  const [carImages, setCarImages] = useState(cars);
+  const [selectedCar, setSelectedCar] = useState(carImages[2]);
+  
+  const back = () => {
+    console.log(carImages.filter((car) => car.id === selectedCar.id - 1)[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={selectedCar.path} />
+      {selectedCar.id === 1 ? (<button disabled>Back</button>) : (<button onClick={back}>Back</button>)}
+      {selectedCar.id === 4 ? (<button disabled>Next</button>) : (<button onClick={() => console.log("yes")}>Next</button>)}
     </div>
   );
 }
-
 export default App;
